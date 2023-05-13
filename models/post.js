5a90f966b8aa6ac09d2a6750e446b4d19a38be8b
fxhanloniii <hanlonfrankiex@gmail.com>
2023-05-13 10:54:28 -0700

    added post model

---
+++ models/post.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+require('../config/connection');
+
+const postSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    caption: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment',
+        },  
+    ],
+    likes: {
+        type: Number,
+        default: 0,
+    },
+});
+
+const Post = mongoose.model('post', postSchema);
+
+module.exports = Post;
