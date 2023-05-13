3ca3a89befa89fed00f73d0f3634187a20ad8b54
fxhanloniii <hanlonfrankiex@gmail.com>
2023-05-13 13:57:14 -0700

    added post/:id route

--- controllers/art.js
+++ controllers/art.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
+const User = require('../models/user');
 const { handleValidateOwnership, requireToken } = require('../middleware/auth');
 
 router.post('/post', requireToken, async(req, res, next) => {
@@ -15,8 +16,25 @@ router.post('/post', requireToken, async(req, res, next) => {
         res.status(201).json(newPost);
     } catch (err) {
         res.status(400).json({
-            error: err.message,
+            error: err.message
         });
+    }
+});
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postId = req.params.id;
+        console.log(postId)
+        const post = await Post.findById(postId);
+        const user = await User.findById(post.user)
+        if (!post) {
+            return res.status(404).json({ error: 'Post not found'});
+        }
+        console.log(post)
+        console.log(user.username)
+        res.json({post, username: user.username});
+    } catch (err) {
+        res.status(400).json({ error: err.message })
     }
 });
 
